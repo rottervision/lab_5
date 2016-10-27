7ae6e154d9e4549d261cf01bd099e2a8e4027acd
rottervision <dave@rottervision.com>
2016-10-27 16:04:44 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 alert('hi');
 
+// my first number is 55 Fahrenheit
 var far = 55;
-
 var far = far-32;
-
 var far = far*.5556
 
-
 console.log (far);
+
+// my first number is 55 Fahrenheit -> 12.7788 Celsius
+
+
+// bonus use strings to help the user understand what your program is doing.
+console.log ("My program is converting 55 Fahrenheit -> 12.7788 Celsius and 40 Celsius -> 104 Fahrenheit")
+
+
+// add the conversion form Celsius to Fahrenheit
+// here I am going to take a new variable and name it cel , value 40 
+
+var cel = 40;
+var cel = cel*1.8;
+var cel = cel+32;
+
+console.log (cel);
+
+// 40 Celsius -> 104 Fahrenheit
+
+
+
+
+
+
+
+
